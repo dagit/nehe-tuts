406d4ac13ae73d3591cffb1846997f84ccd9cf2b
Jason Dagit <dagit@codersbase.com>
2005-08-17 08:28:56 +0000

    lesson10 cleanup

--- lesson10.hs
+++ lesson10.hs
@@ -4,27 +4,12 @@
 
 module Main where
 
-import System.Exit
-import Graphics.Rendering.OpenGL
-import Graphics.Rendering.OpenGL.GL 
-import Graphics.Rendering.OpenGL.GLU
-import Graphics.UI.GLUT
-import Control.Concurrent
-import Data.IORef
-import System.IO
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Utils
-import Foreign.Storable
-import Foreign.Ptr
-import Data.Word
-import Data.Int
-import Control.Monad
-import GHC.IOBase
-
-data Endian = LittleEndian | BigEndian
-              deriving (Eq, Ord, Show)
-
-data Image = Image Size (PixelData Word8)
+import Graphics.UI.GLUT 
+import System.Exit ( exitWith, ExitCode(..) )
+import System.IO ( openFile, IOMode(..), hGetContents )
+import Data.IORef ( IORef, newIORef, writeIORef, readIORef, modifyIORef )
+import Util ( Image(..), bitmapLoad )
+import Monad ( when )
 
 type Sector = [Tri]
 
@@ -35,6 +20,7 @@ data Vert = Vert { vertPoint :: Vertex3 GLfloat,
                    vertTx, vertTy :: GLfloat }
             deriving (Eq, Ord, Show)
 
+piover180 :: GLfloat
 piover180 = 0.0174532925
 
 data Global = Global { xrot, yrot, xspeed, yspeed, walkbias, walkbiasangle,
@@ -42,7 +28,7 @@ data Global = Global { xrot, yrot, xspeed, yspeed, walkbias, walkbiasangle,
                        therotate, heading, zdepth :: IORef GLfloat,
                        filterSelector :: IORef Int }
 mkGlobal :: IO Global
-mkGlobal = do xr <- newIORef 0
+mkGlobal = do xr <- newIORef 0  -- please someone find a nicer way...
               yr <- newIORef 0
               xs <- newIORef 0
               ys <- newIORef 0
@@ -85,13 +71,15 @@ setupWorld = do h <- openFile "Data/world.txt" ReadMode
   readTris (l1:l2:l3:ls) = Tri (readVert (words l1)) (readVert (words l2))
                                (readVert (words l3)) : readTris ls
   readTris [] = []
+  readTris _  = undefined
   readVert (x:y:z:u:v:[]) = Vert (readVertex (x,y,z)) (read u) (read v)
+  readVert _              = undefined
   readVertex (x,y,z) = Vertex3 (read x) (read y) (read z)
   ignorable ('/':'/':_) = False
   ignorable []          = False
   ignorable _           = True
                 
-
+initGL :: IO [TextureObject]
 initGL = do
   tex <- loadGLTextures
   texture Texture2D $= Enabled
@@ -116,86 +104,7 @@ initGL = do
   flush -- finally, we tell opengl to do it.
   return tex
 
-bitmapLoad :: String -> IO Image
-bitmapLoad f = do
-  handle <- openBinaryFile f ReadMode
-  hSeek handle RelativeSeek 18
-  width <- readInt handle
-  putStrLn ("Width of "++f++": "++show width)
-  height <- readInt handle
-  putStrLn ("Height of "++f++": "++show height)
-  planes <- readShort handle
-  bpp <- readShort handle
-  size <- return (width*height*3)
-  hSeek handle RelativeSeek 24
-  putStrLn ("Planes = "++(show planes))
-  bgrBytes <- (readBytes handle (fromIntegral size) :: IO (Ptr Word8))
-  rgbBytes <- bgr2rgb bgrBytes (fromIntegral size)
-  return (Image (Size (fromIntegral width)
-                      (fromIntegral height)) 
-          (PixelData RGB UnsignedByte rgbBytes))
--- Begin low level Bitmap loading code
-endian :: Endian
-endian = let r = unsafePerformIO (
-                 do w <- allocaBytes 4 (\p -> do pokeElemOff p 0 (0::Word8)
-                                                 pokeElemOff p 1 (1::Word8)
-                                                 pokeElemOff p 2 (2::Word8)
-                                                 pokeElemOff p 3 (3::Word8)
-                                                 peek (castPtr p) :: IO Int32)
-                    return w)
-         in case r of 50462976 -> LittleEndian
-                      66051    -> BigEndian
-                      _        -> undefined
-
-bgr2rgb :: Ptr Word8 -> Int -> IO (Ptr Word8)
-bgr2rgb p n = mapM_ (\i -> do b <- peekElemOff p (i+0)
-                              g <- peekElemOff p (i+1)
-                              r <- peekElemOff p (i+2)
-                              pokeElemOff p (i+0) r
-                              pokeElemOff p (i+1) g
-                              pokeElemOff p (i+2) b) [0,3..n-3] 
-              >> return p
-                  
-readStorable :: Storable a => Handle -> Int -> IO (Ptr a)
-readStorable h n = do p <- mallocBytes n
-                      hGetBuf h p n
-                      return p
-
--- This is only needed if you're on PowerPC instead of x86
--- if you are on x86 use the following:
--- reverseBytes p _ = return p
-reverseBytes :: Ptr Word8 -> Int -> IO (Ptr Word8)
-reverseBytes p n | endian == BigEndian = 
-                   do p' <- mallocBytes n
-                      mapM_ (\i -> peekElemOff p i >>= pokeElemOff p' (n-i-1)) 
-                            [0..n-1]
-                      return p'
-                 | endian == LittleEndian = do p' <- mallocBytes n
-                                               copyBytes p' p n
-                                               return p'
-                            
-readBytes :: Storable a => Handle -> Int -> IO (Ptr a)
-readBytes h n = do p <- mallocBytes n
-                   hGetBuf h p n
-                   return p
-
-readShort :: Handle -> IO Word16
-readShort h = do p <- readBytes h 2 :: IO (Ptr Word8)
-                 p' <- reverseBytes (castPtr p) 2
-                 free p
-                 r <- peek (castPtr p')
-                 free p'
-                 return r
-
-readInt :: Handle -> IO Int32
-readInt h = do p <- readBytes h 4 :: IO (Ptr Word8)
-               p' <- reverseBytes (castPtr p) 4
-               free p
-               r <- peek (castPtr p')
-               free p'
-               return r
--- End low level bitmap loading code
-
+loadGLTextures :: IO [TextureObject]
 loadGLTextures = do
   (Image (Size w h) pd) <- bitmapLoad "Data/mud.bmp"
   texNames <- (genObjectNames 3)
@@ -214,6 +123,7 @@ loadGLTextures = do
   build2DMipmaps Texture2D RGB' w h pd
   return texNames
 
+resizeScene :: Size -> IO ()
 resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
 resizeScene s@(Size width height) = do
   viewport   $= (Position 0 0, s)
@@ -223,6 +133,7 @@ resizeScene s@(Size width height) = do
   matrixMode $= Modelview 0
   flush
 
+drawScene :: [TextureObject] -> Sector -> Global -> IO ()
 drawScene texs sector globals = do
   clear [ColorBuffer, DepthBuffer] -- clear the screen and the depth bufer
   loadIdentity  -- reset view
@@ -259,7 +170,6 @@ updateIORef ioref f = do ref <- ioref
                          r <- readIORef ref
                          print r
                          writeIORef ref (f r)
---updateIORef ioref f = ioref >>= (flip modifyIORef) f
 
 keyPressed :: Global -> KeyboardMouseCallback
 -- 27 is ESCAPE
@@ -273,7 +183,7 @@ keyPressed _ (Char 'b') Down _ _  = do
 keyPressed g (Char 'f') Down _ _ = modifyIORef (filterSelector g) (\x -> x+1 `mod` 3)
 keyPressed g (Char 'F') Down x y = keyPressed g (Char 'f') Down x y
 keyPressed g (Char 'L') Down x y = keyPressed g (Char 'l') Down x y
-keyPressed g (Char 'l') Down _ _ = do
+keyPressed _ (Char 'l') Down _ _ = do
   l <- get lighting
   if l == Enabled
      then lighting $= Disabled
@@ -286,14 +196,14 @@ keyPressed g (SpecialKey KeyUp) Down _ _ = do
   modifyIORef (zpos g) (subtract ((cos (h * piover180))*0.05))
   modifyIORef (walkbiasangle g) (\w -> (w+10) `fmod` 360)
   wba <- readIORef (walkbiasangle g)
-  modifyIORef (walkbias g) (\x -> ((sin (wba * piover180))/20))
+  modifyIORef (walkbias g) (\_ -> ((sin (wba * piover180))/20))
 keyPressed g (SpecialKey KeyDown) Down _  _ = do
   h <- readIORef (yrot g)
   modifyIORef (xpos g) (+ (sin (h * piover180))*0.05)
   modifyIORef (zpos g) (+ (cos (h * piover180))*0.05)
   modifyIORef (walkbiasangle g) (\w -> (w-10) `fmod` 360)
   wba <- readIORef (walkbiasangle g)
-  modifyIORef (walkbias g) (\x -> ((sin (wba * piover180))/20))
+  modifyIORef (walkbias g) (\_ -> ((sin (wba * piover180))/20))
 keyPressed g (SpecialKey KeyPageUp) Down _ _ = do
   modifyIORef (zdepth g) (subtract 0.2)
   modifyIORef (lookupdown g) (subtract 0.2)
@@ -303,13 +213,15 @@ keyPressed g (SpecialKey KeyPageDown) Down _ _ = do
 
 keyPressed _ _           _    _ _ = return ()
 
-fmod x m = (fromIntegral ((floor x) `mod` m)) + (x - (fromIntegral (floor x)))
+fmod :: RealFrac a => a -> Int -> a
+fmod x m = (fromIntegral ((floor x :: Int) `mod` m)) + 
+           (x - (fromIntegral (floor x :: Int)))
 
+main :: IO ()
 main = do
      -- Initialize GLUT state - glut will take any command line arguments
      -- that pertain to it or X windows -- look at its documentation at
      -- http://reality.sgi.com/mjk/spec3/spec3.html
---     bitmapLoad "Data/lesson06/NeHe.bmp"
      sector <- setupWorld
 
      getArgsAndInitialize 
