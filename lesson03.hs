99ba3a4dea609ff0cb6c1c307593a4a35bceed49
Jason Dagit <dagitj@gmail.com>
2011-03-20 17:54:51 -0700

    lesson03 is now updated

--- lesson03.hs
+++ lesson03.hs
@@ -4,98 +4,119 @@
 
 module Main where
 
-import Graphics.UI.GLUT
+import qualified Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL.Raw ( glClearColor, glClearDepth
+                                     , glEnable, gl_DEPTH_TEST
+                                     , glDepthFunc, gl_LEQUAL
+                                     , glHint, gl_PERSPECTIVE_CORRECTION_HINT
+                                     , gl_NICEST, glViewport, glFlush
+                                     , glClear, gl_COLOR_BUFFER_BIT
+                                     , gl_DEPTH_BUFFER_BIT, glLoadIdentity
+                                     , glTranslatef, glBegin, gl_TRIANGLES
+                                     , glVertex3f, glEnd, gl_QUADS
+                                     , glShadeModel, gl_SMOOTH, gl_PROJECTION
+                                     , glMatrixMode, gl_MODELVIEW, glColor3f
+                                     )
+import Graphics.Rendering.GLU.Raw ( gluPerspective )
+import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
+import Control.Monad ( forever )
 
 initGL :: IO ()
 initGL = do
-  clearColor $= Color4 0 0 0 0 -- Clear the background color to black
-  clearDepth $= 1 -- enables clearing of the depth buffer
-  depthFunc  $= Just Less -- type of depth test
-  shadeModel $= Smooth -- enables smooth color shading
-  matrixMode $= Projection
-  loadIdentity  -- reset projection matrix
-  Size width height <- get windowSize
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100 -- calculate the aspect ratio of the window
-  matrixMode $= Modelview 0
+  glShadeModel gl_SMOOTH -- enables smooth color shading
+  glClearColor 0 0 0 0 -- Clear the background color to black
+  glClearDepth 1 -- enables clearing of the depth buffer
+  glEnable gl_DEPTH_TEST
+  glDepthFunc gl_LEQUAL  -- type of depth test
+  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
 
-  flush -- finally, we tell opengl to do it.
-
-resizeScene :: Size -> IO ()
-resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
-resizeScene s@(Size width height) = do
-  viewport $= (Position 0 0, s)
-  matrixMode $= Projection
-  loadIdentity
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100
-  matrixMode $= Modelview 0
-  flush
+resizeScene :: GLFW.WindowSizeCallback
+resizeScene w     0      = resizeScene w 1 -- prevent divide by zero
+resizeScene width height = do
+  glViewport 0 0 (fromIntegral width) (fromIntegral height)
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100
+  glMatrixMode gl_MODELVIEW
+  glLoadIdentity
+  glFlush
 
 drawScene :: IO ()
 drawScene = do
-  clear [ColorBuffer, DepthBuffer] -- clear the screen and the depth bufer
-  loadIdentity  -- reset view
+  -- clear the screen and the depth buffer
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  glLoadIdentity -- reset view
 
-  translate (Vector3 (-1.5) 0 (-6.0::GLfloat)) --Move left 1.5 Units and into the screen 6.0
+  glTranslatef (-1.5) 0 (-6.0) --Move left 1.5 Units and into the screen 6.0
   
   -- draw a triangle (in smooth coloring mode)
-  renderPrimitive Polygon $  -- start drawing a polygon
-    do
-       color  (Color3 1 0 (0::GLfloat))          -- set The color to Red
-       vertex (Vertex3  0      1  (0::GLfloat))  -- top
-       color  (Color3 0 1 (0::GLfloat))          -- set The color to Green
-       vertex (Vertex3  1   (-1)  (0::GLfloat))  -- bottom right
-       color  (Color3 0 0 (1::GLfloat))          -- set The color to Blue
-       vertex (Vertex3 (-1) (-1)  (0::GLfloat))  -- bottom left
+  glBegin gl_TRIANGLES
+  glColor3f    1    0  0
+  glVertex3f   0    1  0
+  glColor3f    0    1  0
+  glVertex3f   1  (-1) 0
+  glColor3f    0    0  1
+  glVertex3f (-1) (-1) 0
+  glEnd
 
-  translate (Vector3 3 0 (0::GLfloat))  -- move right three units
+  glTranslatef 3 0 0  -- move right three units
 
-  color (Color3 0.5 0.5 (1::GLfloat))  -- set color to a blue shade
-  renderPrimitive Quads $  -- start drawing a polygon (4 sided)
-    do
-       vertex (Vertex3 (-1)    1  (0::GLfloat))  -- top left
-       vertex (Vertex3  1      1  (0::GLfloat))  -- top right
-       vertex (Vertex3  1   (-1)  (0::GLfloat))  -- bottom right
-       vertex (Vertex3 (-1) (-1)  (0::GLfloat))  -- bottom left
+  glColor3f 0.5 0.5 1  -- set color to a blue shade
+  glBegin gl_QUADS -- start drawing a polygon (4 sided)
+  glVertex3f (-1)   1  0 -- top left
+  glVertex3f   1    1  0 -- top right
+  glVertex3f   1  (-1) 0 -- bottom right
+  glVertex3f (-1) (-1) 0 -- bottom left
+  glEnd
   
-  -- since this is double buffered, swap the buffers to display what was just
-  -- drawn
-  swapBuffers
-  flush
+  glFlush
 
-keyPressed :: KeyboardMouseCallback
--- 27 is ESCAPE
-keyPressed (Char '\27') Down _ _ = exitWith ExitSuccess
-keyPressed _            _    _ _ = return ()
+shutdown :: GLFW.WindowCloseCallback
+shutdown = do
+  GLFW.closeWindow
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return True
+
+keyPressed :: GLFW.KeyCallback
+keyPressed GLFW.KeyEsc True = shutdown >> return ()
+keyPressed _           _    = return ()
 
 main :: IO ()
 main = do
-     -- Initialize GLUT state - glut will take any command line arguments
-     -- that pertain to it or X windows -- look at its documentation at
-     -- http://reality.sgi.com/mjk/spec3/spec3.html
-     _ <- getArgsAndInitialize
+     True <- GLFW.initialize
      -- select type of display mode:
      -- Double buffer
      -- RGBA color
      -- Alpha components supported
      -- Depth buffer
-     initialDisplayMode $= [ DoubleBuffered, RGBAMode, WithDepthBuffer, 
-                             WithAlphaComponent ]
-     -- get a 640 x 480 window
-     initialWindowSize $= Size 800 600
-     -- window starts at upper left corner of the screen
-     initialWindowPosition $= Position 0 0
+     let dspOpts = GLFW.defaultDisplayOptions
+                     -- get a 800 x 600 window
+                     { GLFW.displayOptions_width  = 800
+                     , GLFW.displayOptions_height = 600
+                     -- Set depth buffering and RGBA colors
+                     , GLFW.displayOptions_numRedBits   = 8
+                     , GLFW.displayOptions_numGreenBits = 8
+                     , GLFW.displayOptions_numBlueBits  = 8
+                     , GLFW.displayOptions_numAlphaBits = 8
+                     , GLFW.displayOptions_numDepthBits = 1
+                     -- , GLFW.displayOptions_displayMode = GLFW.Fullscreen
+                     }
      -- open a window
-     _ <- createWindow "Jeff Molofee's GL Code Tutorial ... NeHe '99"
+     True <- GLFW.openWindow dspOpts
+     -- window starts at upper left corner of the screen
+     GLFW.setWindowPosition 0 0
+     GLFW.setWindowTitle "Jeff Molofee's GL Code Tutorial ... NeHe '99"
      -- register the function to do all our OpenGL drawing
-     displayCallback $= drawScene
-     -- go fullscreen. This is as soon as possible.
-     fullScreen
+     GLFW.setWindowRefreshCallback drawScene
      -- register the funciton called when our window is resized
-     reshapeCallback $= Just resizeScene
+     GLFW.setWindowSizeCallback resizeScene
      -- register the function called when the keyboard is pressed.
-     keyboardMouseCallback $= Just keyPressed
+     GLFW.setKeyCallback keyPressed
+     GLFW.setWindowCloseCallback shutdown
      -- initialize our window.
      initGL
      -- start event processing engine
-     mainLoop
+     forever $ GLFW.waitEvents >> drawScene >> GLFW.swapBuffers
