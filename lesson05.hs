af0f906e1eade7cab375a6282728861f22fe05ce
Jason Dagit <dagitj@gmail.com>
2011-03-20 19:02:28 -0700

    update lesson05

--- lesson05.hs
+++ lesson05.hs
@@ -4,9 +4,26 @@
 
 module Main where
 
-import Graphics.UI.GLUT
+import qualified Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL.Raw ( glClearColor, glClearDepth
+                                     , glEnable, gl_DEPTH_TEST
+                                     , glDepthFunc, gl_LEQUAL
+                                     , glHint, gl_PERSPECTIVE_CORRECTION_HINT
+                                     , gl_NICEST, glViewport, glFlush
+                                     , glClear, gl_COLOR_BUFFER_BIT
+                                     , gl_DEPTH_BUFFER_BIT, glLoadIdentity
+                                     , glTranslatef, glBegin, gl_TRIANGLES
+                                     , glVertex3f, glEnd, gl_QUADS
+                                     , glShadeModel, gl_SMOOTH, gl_PROJECTION
+                                     , glMatrixMode, gl_MODELVIEW, GLfloat
+                                     , glRotatef, glColor3f
+                                     )
+import Graphics.Rendering.GLU.Raw ( gluPerspective )
+import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
-import Data.IORef ( IORef, newIORef )
+import Control.Monad ( forever )
+import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import Control.Concurrent ( threadDelay )
 
 tincrement, qincrement :: GLfloat
 tincrement = 0.2
@@ -14,157 +31,167 @@ qincrement = -0.15
 
 initGL :: IO ()
 initGL = do
-  clearColor $= Color4 0 0 0 0 -- Clear the background color to black
-  clearDepth $= 1 -- enables clearing of the depth buffer
-  depthFunc  $= Just Less -- type of depth test
-  shadeModel $= Smooth -- enables smooth color shading
-  matrixMode $= Projection
-  loadIdentity  -- reset projection matrix
-  Size width height <- get windowSize
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100 -- calculate the aspect ratio of the window
-  matrixMode $= Modelview 0
+  glShadeModel gl_SMOOTH -- enables smooth color shading
+  glClearColor 0 0 0 0 -- Clear the background color to black
+  glClearDepth 1 -- enables clearing of the depth buffer
+  glEnable gl_DEPTH_TEST
+  glDepthFunc gl_LEQUAL  -- type of depth test
+  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
 
-  flush -- finally, we tell opengl to do it.
-
-resizeScene :: Size -> IO ()
-resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
-resizeScene s@(Size width height) = do
-  viewport   $= (Position 0 0, s)
-  matrixMode $= Projection
-  loadIdentity
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100
-  matrixMode $= Modelview 0
-  flush
+resizeScene :: GLFW.WindowSizeCallback
+resizeScene w     0      = resizeScene w 1 -- prevent divide by zero
+resizeScene width height = do
+  glViewport 0 0 (fromIntegral width) (fromIntegral height)
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100
+  glMatrixMode gl_MODELVIEW
+  glLoadIdentity
+  glFlush
 
 drawScene :: IORef GLfloat -> IORef GLfloat -> IO ()
 drawScene rtri rquad = do
-  clear [ColorBuffer, DepthBuffer] -- clear the screen and the depth bufer
-  loadIdentity  -- reset view
+  -- clear the screen and the depth buffer
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  glLoadIdentity  -- reset view
 
-  translate (Vector3 (-1.5) 0 (-6.0::GLfloat)) --Move left 1.5 Units and into the screen 6.0
-  rt <- get rtri
-  rotate rt (Vector3 0 1 (0::GLfloat)) -- Rotate the triangle on the Y axis
+  glTranslatef (-1.5) 0 (-6.0) --Move left 1.5 Units and into the screen 6.0
+  rt <- readIORef rtri
+  glRotatef rt 0 1 0 -- Rotate the triangle on the Y axis
 
   -- draw a triangle (in smooth coloring mode)
-  renderPrimitive Triangles $  -- start drawing a polygon
-    do -- first the front
-       color  (Color3 1 0 (0::GLfloat))          -- set The color to Red
-       vertex (Vertex3  0      1  (0::GLfloat))  -- top of triangle (front)
-       color  (Color3 0 1 (0::GLfloat))          -- set The color to Green
-       vertex (Vertex3 (-1) (-1)  (1::GLfloat))  -- left of triangle (front)
-       color  (Color3 0 0 (1::GLfloat))          -- set The color to Blue
-       vertex (Vertex3  1   (-1)  (1::GLfloat))  -- right of triangle (front)
-       -- now the right
-       color  (Color3 1 0 (0::GLfloat))          -- set The color to Red
-       vertex (Vertex3  0      1  (0::GLfloat))  -- top of triangle (right)
-       color  (Color3 0 0 (1::GLfloat))          -- set The color to Blue
-       vertex (Vertex3  1   (-1)  (1::GLfloat))  -- left of triangle (right)
-       color  (Color3 0 1 (0::GLfloat))          -- set The color to Green
-       vertex (Vertex3  1  (-1)  (-1::GLfloat))  -- right of triangle (front)
-       -- now the back
-       color  (Color3 1 0 (0::GLfloat))          -- set The color to Red
-       vertex (Vertex3  0      1  (0::GLfloat))  -- top of triangle (back)
-       color  (Color3 0 1 (0::GLfloat))          -- set The color to Green
-       vertex (Vertex3  1 (-1)   (-1::GLfloat))  -- left of triangle (back)
-       color  (Color3 0 0 (1::GLfloat))          -- set The color to Blue
-       vertex (Vertex3 (-1) (-1) (-1::GLfloat))  -- right of triangle (back)
-       -- now the left
-       color  (Color3 1 0 (0::GLfloat))          -- set The color to Red
-       vertex (Vertex3  0      1  (0::GLfloat))  -- top of triangle (left)
-       color  (Color3 0 0 (1::GLfloat))          -- set The color to Blue
-       vertex (Vertex3 (-1) (-1) (-1::GLfloat))  -- left of triangle (left)
-       color  (Color3 0 1 (0::GLfloat))          -- set The color to Green
-       vertex (Vertex3 (-1) (-1)  (1::GLfloat))  -- right of triangle (left)
-
+  glBegin gl_TRIANGLES  -- start drawing a polygon
+  -- first the front
+  glColor3f    1    0    0  -- set The color to Red
+  glVertex3f   0    1    0  -- top of triangle (front)
+  glColor3f    0    1    0  -- set The color to Green
+  glVertex3f (-1) (-1)   1  -- left of triangle (front)
+  glColor3f    0    0    1  -- set The color to Blue
+  glVertex3f   1  (-1)   1  -- right of triangle (front)
+  -- now the right
+  glColor3f    1    0    0  -- set The color to Red
+  glVertex3f   0    1    0  -- top of triangle (right)
+  glColor3f    0    0    1  -- set The color to Blue
+  glVertex3f   1  (-1)   1  -- left of triangle (right)
+  glColor3f    0    1    0  -- set The color to Green
+  glVertex3f   1  (-1) (-1) -- right of triangle (front)
+  -- now the back
+  glColor3f    1    0    0  -- set The color to Red
+  glVertex3f   0    1    0  -- top of triangle (back)
+  glColor3f    0    1    0  -- set The color to Green
+  glVertex3f   1  (-1) (-1) -- left of triangle (back)
+  glColor3f    0    0    1  -- set The color to Blue
+  glVertex3f (-1) (-1) (-1) -- right of triangle (back)
+  -- now the left
+  glColor3f    1    0    0  -- set The color to Red
+  glVertex3f   0    1    0  -- top of triangle (left)
+  glColor3f    0    0    1  -- set The color to Blue
+  glVertex3f (-1) (-1) (-1) -- left of triangle (left)
+  glColor3f    0    1    0  -- set The color to Green
+  glVertex3f (-1) (-1)   1  -- right of triangle (left)
+  glEnd
        
-  loadIdentity
-  translate (Vector3 1.5 0 (-7::GLfloat))  -- move right three units
-  rq <- get rquad
-  rotate rq (Vector3 1 1 (1::GLfloat)) -- rotate the quad on the x axis
+  glLoadIdentity
+  glTranslatef 1.5 0 (-7)  -- move right three units
+  rq <- readIORef rquad
+  glRotatef rq 1 1 1 -- rotate the quad on the x axis
 
-  renderPrimitive Quads $  -- start drawing a polygon (4 sided)
-    do -- first the top
-       color (Color3 0 1 (0::GLfloat))  -- set color to green
-       vertex (Vertex3  1    1   (-1::GLfloat))  -- top right of quad (Top)
-       vertex (Vertex3 (-1)  1   (-1::GLfloat))  -- top left of quad (Top)
-       vertex (Vertex3 (-1)  1    (1::GLfloat))  -- bottom left of quad (Top)
-       vertex (Vertex3  1    1    (1::GLfloat))  -- bottom right of quad (Top)
-       -- now the bottom
-       color (Color3 1 0.5 (0::GLfloat))  -- set color to orage
-       vertex (Vertex3  1   (-1)   (1::GLfloat))  -- top right of quad (Bottom)
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- top left of quad (Bottom)
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom left of quad (Bottom)
-       vertex (Vertex3  1   (-1)  (-1::GLfloat))  -- bottom right of quad (Bottom)
-       -- now the front
-       color (Color3 1 0 (0::GLfloat))  -- set color to red
-       vertex (Vertex3  1      1   (1::GLfloat))  -- top right of quad (Bottom)
-       vertex (Vertex3 (-1)    1   (1::GLfloat))  -- top left of quad (Bottom)
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- bottom left of quad (Bottom)
-       vertex (Vertex3  1   (-1)   (1::GLfloat))  -- bottom right of quad (Bottom)
-       -- now the back
-       color (Color3 1 1 (0::GLfloat))  -- set color to yellow
-       vertex (Vertex3    1 (-1)  (-1::GLfloat))  -- bottom left of quad (Back)
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom right of quad (Back)
-       vertex (Vertex3 (-1)    1  (-1::GLfloat))  -- top right of quad (Back)
-       vertex (Vertex3    1    1  (-1::GLfloat))  -- top left of quad (Back)
-       -- now the left
-       color (Color3 0 0 (1::GLfloat))  -- set color to blue
-       vertex (Vertex3 (-1)    1   (1::GLfloat))  -- top right of quad (Left)
-       vertex (Vertex3 (-1)    1  (-1::GLfloat))  -- top left of quad (Left)
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom left of quad (Left)
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- bottom right of quad (Left)
-       -- now the right
-       color (Color3 1 0 (1::GLfloat))  -- set color to violet
-       vertex (Vertex3 1    1  (-1::GLfloat))  -- top right of quad (Right)
-       vertex (Vertex3 1    1   (1::GLfloat))  -- top left of quad (Right)
-       vertex (Vertex3 1 (-1)   (1::GLfloat))  -- bottom left of quad (Right)
-       vertex (Vertex3 1 (-1)  (-1::GLfloat))  -- bottom right of quad (Right)
+  glBegin gl_QUADS -- start drawing a polygon (4 sided)
+  -- first the top
+  glColor3f    0    1    0  -- set color to green
+  glVertex3f   1    1  (-1) -- top right of quad (Top)
+  glVertex3f (-1)   1  (-1) -- top left of quad (Top)
+  glVertex3f (-1)   1    1  -- bottom left of quad (Top)
+  glVertex3f   1    1    1  -- bottom right of quad (Top)
+  -- now the bottom
+  glColor3f    1  0.5    0  -- set color to orage
+  glVertex3f   1  (-1)   1  -- top right of quad (Bottom)
+  glVertex3f (-1) (-1)   1  -- top left of quad (Bottom)
+  glVertex3f (-1) (-1) (-1) -- bottom left of quad (Bottom)
+  glVertex3f   1  (-1) (-1) -- bottom right of quad (Bottom)
+  -- now the front
+  glColor3f    1    0    0  -- set color to red
+  glVertex3f   1    1    1  -- top right of quad (Bottom)
+  glVertex3f (-1)   1    1  -- top left of quad (Bottom)
+  glVertex3f (-1) (-1)   1  -- bottom left of quad (Bottom)
+  glVertex3f   1  (-1)   1  -- bottom right of quad (Bottom)
+  -- now the back
+  glColor3f    1    1    0  -- set color to yellow
+  glVertex3f   1  (-1) (-1) -- bottom left of quad (Back)
+  glVertex3f (-1) (-1) (-1) -- bottom right of quad (Back)
+  glVertex3f (-1)   1  (-1) -- top right of quad (Back)
+  glVertex3f   1    1  (-1) -- top left of quad (Back)
+  -- now the left
+  glColor3f    0    0    1  -- set color to blue
+  glVertex3f (-1)   1    1  -- top right of quad (Left)
+  glVertex3f (-1)   1  (-1) -- top left of quad (Left)
+  glVertex3f (-1) (-1) (-1) -- bottom left of quad (Left)
+  glVertex3f (-1) (-1)   1  -- bottom right of quad (Left)
+  -- now the right
+  glColor3f    1    0    1  -- set color to violet
+  glVertex3f   1    1  (-1) -- top right of quad (Right)
+  glVertex3f   1    1    1  -- top left of quad (Right)
+  glVertex3f   1  (-1)   1  -- bottom left of quad (Right)
+  glVertex3f   1  (-1) (-1) -- bottom right of quad (Right)
+  glEnd
   
-  rtri  $= rt + tincrement --increase the rotation angle for the triangle
-  rquad $= rq + qincrement --increase the rotation angle for the quad
+  --increase the rotation angle for the triangle
+  writeIORef rtri  $! rt + tincrement
+  --increase the rotation angle for the quad
+  writeIORef rquad $! rq + qincrement
 
-  -- since this is double buffered, swap the buffers to display what was just
-  -- drawn
-  swapBuffers
-  flush
+  glFlush
 
-keyPressed :: KeyboardMouseCallback
--- 27 is ESCAPE
-keyPressed (Char '\27') Down _ _ = exitWith ExitSuccess
-keyPressed _            _    _ _ = return ()
+shutdown :: GLFW.WindowCloseCallback
+shutdown = do
+  GLFW.closeWindow
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return True
+
+keyPressed :: GLFW.KeyCallback
+keyPressed GLFW.KeyEsc True = shutdown >> return () 
+keyPressed _           _    = return ()
 
 main :: IO ()
 main = do
-     -- Initialize GLUT state - glut will take any command line arguments
-     -- that pertain to it or X windows -- look at its documentation at
-     -- http://reality.sgi.com/mjk/spec3/spec3.html
-     _ <- getArgsAndInitialize
+     True <- GLFW.initialize
      -- select type of display mode:
      -- Double buffer
      -- RGBA color
      -- Alpha components supported
      -- Depth buffer
-     initialDisplayMode $= [ DoubleBuffered, RGBAMode, WithDepthBuffer, 
-                             WithAlphaComponent ]
-     -- get a 640 x 480 window
-     initialWindowSize $= Size 800 600
-     -- window starts at upper left corner of the screen
-     initialWindowPosition $= Position 0 0
+     let dspOpts = GLFW.defaultDisplayOptions
+                     -- get a 800 x 600 window
+                     { GLFW.displayOptions_width  = 800
+                     , GLFW.displayOptions_height = 600
+                     -- Set depth buffering and RGBA colors
+                     , GLFW.displayOptions_numRedBits   = 8
+                     , GLFW.displayOptions_numGreenBits = 8
+                     , GLFW.displayOptions_numBlueBits  = 8
+                     , GLFW.displayOptions_numAlphaBits = 8
+                     , GLFW.displayOptions_numDepthBits = 1
+                     -- , GLFW.displayOptions_displayMode = GLFW.Fullscreen
+                     }
      -- open a window
-     _ <- createWindow "Jeff Molofee's GL Code Tutorial ... NeHe '99"
+     True <- GLFW.openWindow dspOpts
+     -- window starts at upper left corner of the screen
+     GLFW.setWindowPosition 0 0
+     GLFW.setWindowTitle "Jeff Molofee's GL Code Tutorial ... NeHe '99"
      -- register the function to do all our OpenGL drawing
      rt <- newIORef 0
      rq <- newIORef 0
-     displayCallback $= (drawScene rt rq)
-     -- go fullscreen. This is as soon as possible.
-     fullScreen
-     -- even if there are no events, redraw our gl scene
-     idleCallback $= Just (drawScene rt rq)
+     GLFW.setWindowRefreshCallback (drawScene rt rq)
      -- register the funciton called when our window is resized
-     reshapeCallback $= Just resizeScene
+     GLFW.setWindowSizeCallback resizeScene
      -- register the function called when the keyboard is pressed.
-     keyboardMouseCallback $= Just keyPressed
+     GLFW.setKeyCallback keyPressed
+     GLFW.setWindowCloseCallback shutdown
      -- initialize our window.
      initGL
      -- start event processing engine
-     mainLoop
+     forever $ do
+       threadDelay 1
+       drawScene rt rq
+       GLFW.swapBuffers
