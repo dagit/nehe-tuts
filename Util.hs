3921a95766377ca95e689125ad0f408b9ce7a072
Jason Dagit <dagitj@gmail.com>
2011-03-20 23:58:02 -0700

    Update lesson06

--- Util.hs
+++ Util.hs
@@ -1,91 +1,48 @@
 module Util ( bitmapLoad, Image(..) ) where
 
-import Data.Word ( Word8, Word16 )
-import Data.Int ( Int32 )
-import Foreign ( Ptr, allocaBytes, pokeElemOff, peekElemOff, peek, castPtr,
-                 Storable, mallocBytes, free, copyBytes )
-import Graphics.Rendering.OpenGL ( PixelData(..), PixelFormat(..), Size(..), 
-                                   DataType(..) )
-import System.IO ( IOMode( ReadMode ), openBinaryFile, hSeek, 
-                   SeekMode( RelativeSeek ), Handle, hGetBuf )
-import System.IO.Unsafe ( unsafePerformIO )
+import Data.Word ( Word8, Word32 )
+import Data.Serialize.Get ( Get, runGet, getWord32le, getWord16le
+                          , skip, getByteString )
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Internal as BSI
+import Foreign ( Ptr, pokeElemOff, peekElemOff, plusPtr
+               , withForeignPtr )
 
-data Endian = LittleEndian | BigEndian
-              deriving (Eq, Ord, Show)
+data Image = Image !Int !Int !BS.ByteString
 
-data Image = Image Size (PixelData Word8)
-
-bitmapLoad :: String -> IO Image
+bitmapLoad :: FilePath -> IO (Maybe Image)
 bitmapLoad f = do
-  handle <- openBinaryFile f ReadMode
-  hSeek handle RelativeSeek 18
-  width <- readInt handle
-  putStrLn ("Width of "++f++": "++show width)
-  height <- readInt handle
-  putStrLn ("Height of "++f++": "++show height)
-  planes <- readShort handle
-  bpp <- readShort handle
-  size <- return (width*height*(fromIntegral bpp `div` 8))
-  hSeek handle RelativeSeek 24
-  putStrLn ("Planes = "++(show planes))
-  bgrBytes <- (readBytes handle (fromIntegral size) :: IO (Ptr Word8))
-  rgbBytes <- bgr2rgb bgrBytes (fromIntegral size)
-  return (Image (Size (fromIntegral width) (fromIntegral height))
-          (PixelData RGB UnsignedByte rgbBytes))
+  bs <- BS.readFile f
+  case runGet getBitmap bs of
+    Left  err -> putStrLn err >> return Nothing
+    Right i@(Image _ _ bytes) -> do
+      let (ptr, offset, len) = BSI.toForeignPtr bytes
+      withForeignPtr ptr $ bgr2rgb len offset
+      return $! Just i
 
-endian :: Endian
-endian = let r = unsafePerformIO (
-                 do w <- allocaBytes 4 (\p -> do pokeElemOff p 0 (0::Word8)
-                                                 pokeElemOff p 1 (1::Word8)
-                                                 pokeElemOff p 2 (2::Word8)
-                                                 pokeElemOff p 3 (3::Word8)
-                                                 peek (castPtr p) :: IO Int32)
-                    return w)
-         in case r of 50462976 -> LittleEndian
-                      66051    -> BigEndian
-                      _        -> undefined
+-- | Returns a bitmap in bgr format.
+getBitmap :: Get Image
+getBitmap = do
+  skip 18
+  width   <- getWord32le
+  height  <- getWord32le
+  _planes <- getWord16le
+  bpp     <- getWord16le
+  let size = fromIntegral $
+              width * height * (fromIntegral (bpp `div` 8) :: Word32) :: Int
+  skip 24
+  bgrBytes <- getByteString size
+  return $! Image (fromIntegral width)
+                  (fromIntegral height)
+                  bgrBytes
 
-bgr2rgb :: Ptr Word8 -> Int -> IO (Ptr Word8)
-bgr2rgb p n = mapM_ (\i -> do b <- peekElemOff p (i+0)
-                              g <- peekElemOff p (i+1)
-                              r <- peekElemOff p (i+2)
-                              pokeElemOff p (i+0) r
-                              pokeElemOff p (i+1) g
-                              pokeElemOff p (i+2) b) [0,3..n-3] 
-              >> return p
-                  
--- This is only needed if you're on PowerPC instead of x86
--- if you are on x86 use the following:
--- reverseBytes p _ = return p
-reverseBytes :: Ptr Word8 -> Int -> IO (Ptr Word8)
-reverseBytes p n | endian == BigEndian = 
-                   do p' <- mallocBytes n
-                      mapM_ (\i -> peekElemOff p i >>= pokeElemOff p' (n-i-1)) 
-                            [0..n-1]
-                      return p'
-                 | endian == LittleEndian = do p' <- mallocBytes n
-                                               copyBytes p' p n
-                                               return p'
-reverseBytes _ _ = undefined
-                            
-readBytes :: Storable a => Handle -> Int -> IO (Ptr a)
-readBytes h n = do p <- mallocBytes n
-                   _ <- hGetBuf h p n
-                   return p
-
-readShort :: Handle -> IO Word16
-readShort h = do p <- readBytes h 2 :: IO (Ptr Word8)
-                 p' <- reverseBytes (castPtr p) 2
-                 free p
-                 r <- peek (castPtr p')
-                 free p'
-                 return r
-
-readInt :: Handle -> IO Int32
-readInt h = do p <- readBytes h 4 :: IO (Ptr Word8)
-               p' <- reverseBytes (castPtr p) 4
-               free p
-               r <- peek (castPtr p')
-               free p'
-               return r
-
+bgr2rgb :: Int -> Int -> Ptr Word8 -> IO ()
+bgr2rgb n o p = do
+  mapM_ (\i -> do
+    b <- peekElemOff p' (i+0)
+    r <- peekElemOff p' (i+2)
+    pokeElemOff p' (i+0) (r::Word8)
+    pokeElemOff p' (i+2) (b::Word8))
+    [0,3..n-3]
+  where
+  p' = p `plusPtr` o
--- lesson06.hs
+++ lesson06.hs
@@ -4,169 +4,211 @@
 
 module Main where
 
-import Graphics.UI.GLUT 
+import qualified Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL.Raw ( glClearColor, glClearDepth
+                                     , glEnable, gl_DEPTH_TEST
+                                     , glDepthFunc, gl_LEQUAL
+                                     , glHint, gl_PERSPECTIVE_CORRECTION_HINT
+                                     , gl_NICEST, glViewport, glFlush
+                                     , glClear, gl_COLOR_BUFFER_BIT
+                                     , gl_DEPTH_BUFFER_BIT, glLoadIdentity
+                                     , glTranslatef, glBegin
+                                     , glVertex3f, glEnd, gl_QUADS
+                                     , glShadeModel, gl_SMOOTH, gl_PROJECTION
+                                     , glMatrixMode, gl_MODELVIEW, GLfloat
+                                     , glRotatef, glTexCoord2f
+                                     , glTexImage2D, gl_TEXTURE_2D, gl_RGB
+                                     , gl_UNSIGNED_BYTE, glTexParameteri
+                                     , gl_TEXTURE_MIN_FILTER, gl_LINEAR
+                                     , gl_TEXTURE_MAG_FILTER, GLuint
+                                     , glGenTextures, glBindTexture
+                                     )
+import Graphics.Rendering.GLU.Raw ( gluPerspective )
+import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
-import Monad ( liftM )
-import Data.IORef ( IORef, newIORef )
+import Control.Monad ( forever )
+import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import Foreign ( withForeignPtr, plusPtr, mallocForeignPtr, peek )
+import qualified Data.ByteString.Internal as BSI
+import Control.Concurrent ( threadDelay )
 import Util ( Image(..), bitmapLoad )
 
-initGL :: IO TextureObject
+initGL :: IO GLuint
 initGL = do
-  tex <- loadGLTextures
-  texture Texture2D $= Enabled
-  clearColor $= Color4 0 0 0 0.5 -- Clear the background color to black
-  clearDepth $= 1 -- enables clearing of the depth buffer
-  depthFunc  $= Just Less -- type of depth test
-  shadeModel $= Smooth -- enables smooth color shading
-  matrixMode $= Projection
-  loadIdentity  -- reset projection matrix
-  Size width height <- get windowSize
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100 -- calculate the aspect ratio of the window
-  matrixMode $= Modelview 0
+  glEnable gl_TEXTURE_2D
+  glShadeModel gl_SMOOTH
+  glClearColor 0 0 0 0
+  glClearDepth 1
+  glEnable gl_DEPTH_TEST
+  glDepthFunc gl_LEQUAL
+  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
+  loadGLTextures
 
-  flush -- finally, we tell opengl to do it.
+loadGLTextures :: IO GLuint
+loadGLTextures = do
+  Just (Image w h pd) <- bitmapLoad "Data/NeHe.bmp"
+  putStrLn $ "Image width  = " ++ show w
+  putStrLn $ "Image height = " ++ show h
+  tex <- mallocForeignPtr >>= \ptr ->
+           withForeignPtr ptr $ \p -> do
+    glGenTextures 1 p
+    peek p :: IO GLuint
+  let (ptr, off, _) = BSI.toForeignPtr pd
+  withForeignPtr ptr $ \p -> do
+    let p' = p `plusPtr` off
+    glBindTexture gl_TEXTURE_2D tex
+    glTexImage2D gl_TEXTURE_2D 0 3
+      (fromIntegral w) (fromIntegral h) 0 gl_RGB gl_UNSIGNED_BYTE
+      p'
+    let glLinear = fromIntegral gl_LINEAR
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MIN_FILTER glLinear
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MAG_FILTER glLinear
   return tex
 
-loadGLTextures :: IO TextureObject
-loadGLTextures = do
-  (Image (Size w h) pd) <- bitmapLoad "Data/NeHe.bmp"
-  texName <- liftM head (genObjectNames 1)
-  textureBinding Texture2D $= Just texName
-  textureFilter  Texture2D $= ((Nearest, Nothing), Nearest)
-  texImage2D Nothing NoProxy 0 RGB' (TextureSize2D w h) 0 pd
-  return texName
+resizeScene :: GLFW.WindowSizeCallback
+resizeScene w     0      = resizeScene w 1 -- prevent divide by zero
+resizeScene width height = do
+  glViewport 0 0 (fromIntegral width) (fromIntegral height)
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100
+  glMatrixMode gl_MODELVIEW
+  glLoadIdentity
+  glFlush
 
-resizeScene :: Size -> IO ()
-resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
-resizeScene s@(Size width height) = do
-  viewport   $= (Position 0 0, s)
-  matrixMode $= Projection
-  loadIdentity
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100
-  matrixMode $= Modelview 0
-  flush
-
-drawScene :: TextureObject -> IORef GLfloat -> IORef GLfloat 
-             -> IORef GLfloat -> IO ()
+drawScene :: GLuint -> IORef GLfloat -> IORef GLfloat 
+          -> IORef GLfloat -> IO ()
 drawScene tex xrot yrot zrot = do
-  clear [ColorBuffer, DepthBuffer] -- clear the screen and the depth bufer
-  loadIdentity  -- reset view
+  -- clear the screen and the depth buffer
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  glLoadIdentity -- reset view
 
-  translate (Vector3 0 0 (-5.0::GLfloat)) --Move left 5 Units into the screen
+  glTranslatef 0 0 (-5.0) --Move left 5 Units into the screen
 
-  xr <- get xrot
-  yr <- get yrot
-  zr <- get zrot
-  rotate xr (Vector3 1 0 (0::GLfloat)) -- Rotate the triangle on the Y axis
-  rotate yr (Vector3 0 1 (0::GLfloat)) -- Rotate the triangle on the Y axis
-  rotate zr (Vector3 0 0 (1::GLfloat)) -- Rotate the triangle on the Y axis
+  xr <- readIORef xrot
+  yr <- readIORef yrot
+  zr <- readIORef zrot
+  glRotatef xr 1 0 0 -- Rotate the triangle on the Y axis
+  glRotatef yr 0 1 0 -- Rotate the triangle on the Y axis
+  glRotatef zr 0 0 1 -- Rotate the triangle on the Y axis
 
-  textureBinding Texture2D $= Just tex
-  renderPrimitive Quads $  -- start drawing a polygon (4 sided)
-    do 
-       -- first the front
-       texCoord (TexCoord2 0 (0::GLfloat)) 
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- bottom left of quad (Front)
-       texCoord (TexCoord2 1 (0::GLfloat)) 
-       vertex (Vertex3  1   (-1)   (1::GLfloat))  -- bottom right of quad (Front)
-       texCoord (TexCoord2 1 (1::GLfloat)) 
-       vertex (Vertex3  1      1   (1::GLfloat))  -- top right of quad (Front)
-       texCoord (TexCoord2 0 (1::GLfloat)) 
-       vertex (Vertex3 (-1)    1   (1::GLfloat))  -- top left of quad (Front)
-       -- now the back
-       texCoord (TexCoord2 1 (0::GLfloat)) 
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom right of quad (Back)
-       texCoord (TexCoord2 1 (1::GLfloat)) 
-       vertex (Vertex3 (-1)    1  (-1::GLfloat))  -- top right of quad (Back)
-       texCoord (TexCoord2 0 (1::GLfloat)) 
-       vertex (Vertex3    1    1  (-1::GLfloat))  -- top left of quad (Back)
-       texCoord (TexCoord2 0 (0::GLfloat)) 
-       vertex (Vertex3    1 (-1)  (-1::GLfloat))  -- bottom left of quad (Back)
-       -- now the top
-       texCoord (TexCoord2 0 (1::GLfloat))
-       vertex (Vertex3 (-1)  1   (-1::GLfloat))  -- top left of quad (Top)
-       texCoord (TexCoord2 0 (0::GLfloat))  
-       vertex (Vertex3 (-1)  1    (1::GLfloat))  -- bottom left of quad (Top)
-       texCoord (TexCoord2 1 (0::GLfloat))  
-       vertex (Vertex3  1    1    (1::GLfloat))  -- bottom right of quad (Top)
-       texCoord (TexCoord2 1 (1::GLfloat))  
-       vertex (Vertex3  1    1   (-1::GLfloat))  -- top right of quad (Top)
-       -- now the bottom
-       texCoord (TexCoord2 1 (1::GLfloat))  
-       vertex (Vertex3  1   (-1)   (1::GLfloat))  -- top right of quad (Bottom)
-       texCoord (TexCoord2 0 (1::GLfloat))  
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- top left of quad (Bottom)
-       texCoord (TexCoord2 0 (0::GLfloat))  
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom left of quad (Bottom)
-       texCoord (TexCoord2 1 (0::GLfloat))  
-       vertex (Vertex3  1   (-1)  (-1::GLfloat))  -- bottom right of quad (Bottom)
-       -- now the right
-       texCoord (TexCoord2 1 (0::GLfloat))  
-       vertex (Vertex3 1 (-1)  (-1::GLfloat))  -- bottom right of quad (Right)
-       texCoord (TexCoord2 1 (1::GLfloat))  
-       vertex (Vertex3 1    1  (-1::GLfloat))  -- top right of quad (Right)
-       texCoord (TexCoord2 0 (1::GLfloat))  
-       vertex (Vertex3 1    1   (1::GLfloat))  -- top left of quad (Right)
-       texCoord (TexCoord2 0 (0::GLfloat))  
-       vertex (Vertex3 1 (-1)   (1::GLfloat))  -- bottom left of quad (Right)
-       -- now the left
-       texCoord (TexCoord2 0 (0::GLfloat))  
-       vertex (Vertex3 (-1) (-1)  (-1::GLfloat))  -- bottom left of quad (Left)
-       texCoord (TexCoord2 1 (0::GLfloat))  
-       vertex (Vertex3 (-1)    1  (-1::GLfloat))  -- top left of quad (Left)
-       texCoord (TexCoord2 1 (1::GLfloat))  
-       vertex (Vertex3 (-1)    1   (1::GLfloat))  -- top right of quad (Left)
-       texCoord (TexCoord2 0 (1::GLfloat))  
-       vertex (Vertex3 (-1) (-1)   (1::GLfloat))  -- bottom right of quad (Left)
+  glBindTexture gl_TEXTURE_2D tex
+
+  glBegin gl_QUADS -- start drawing a polygon (4 sided)
+  -- first the front
+  glTexCoord2f   0    0
+  glVertex3f   (-1) (-1)    1  -- bottom left of quad (Front)
+  glTexCoord2f   1    0
+  glVertex3f     1  (-1)    1  -- bottom right of quad (Front)
+  glTexCoord2f   1    1 
+  glVertex3f     1    1     1  -- top right of quad (Front)
+  glTexCoord2f   0    1 
+  glVertex3f   (-1)   1     1  -- top left of quad (Front)
+  -- now the back
+  glTexCoord2f   1    0 
+  glVertex3f   (-1) (-1) (-1)  -- bottom right of quad (Back)
+  glTexCoord2f   1    1 
+  glVertex3f   (-1)   1  (-1)  -- top right of quad (Back)
+  glTexCoord2f   0    1 
+  glVertex3f     1    1  (-1)  -- top left of quad (Back)
+  glTexCoord2f   0    0 
+  glVertex3f     1  (-1) (-1)  -- bottom left of quad (Back)
+  -- now the top
+  glTexCoord2f   0    1
+  glVertex3f   (-1)   1  (-1)  -- top left of quad (Top)
+  glTexCoord2f   0    0  
+  glVertex3f   (-1)   1    1   -- bottom left of quad (Top)
+  glTexCoord2f   1    0  
+  glVertex3f     1    1    1   -- bottom right of quad (Top)
+  glTexCoord2f   1    1  
+  glVertex3f     1    1  (-1)  -- top right of quad (Top)
+  -- now the bottom
+  glTexCoord2f   1    1  
+  glVertex3f     1  (-1)   1   -- top right of quad (Bottom)
+  glTexCoord2f   0    1  
+  glVertex3f   (-1) (-1)   1   -- top left of quad (Bottom)
+  glTexCoord2f   0    0 
+  glVertex3f   (-1) (-1) (-1)  -- bottom left of quad (Bottom)
+  glTexCoord2f   1    0  
+  glVertex3f     1  (-1) (-1)  -- bottom right of quad (Bottom)
+  -- now the right
+  glTexCoord2f   1    0  
+  glVertex3f     1  (-1) (-1)  -- bottom right of quad (Right)
+  glTexCoord2f   1    1  
+  glVertex3f     1    1  (-1)  -- top right of quad (Right)
+  glTexCoord2f   0    1  
+  glVertex3f     1    1    1   -- top left of quad (Right)
+  glTexCoord2f   0    0  
+  glVertex3f     1  (-1)   1   -- bottom left of quad (Right)
+  -- now the left
+  glTexCoord2f   0    0  
+  glVertex3f   (-1) (-1) (-1)  -- bottom left of quad (Left)
+  glTexCoord2f   1    0  
+  glVertex3f   (-1)   1  (-1)  -- top left of quad (Left)
+  glTexCoord2f   1    1  
+  glVertex3f   (-1)   1    1   -- top right of quad (Left)
+  glTexCoord2f   0    1 
+  glVertex3f   (-1) (-1)   1   -- bottom right of quad (Left)
   
-  xrot $= xr + 0.3
-  yrot $= yr + 0.2
-  zrot $= zr + 0.4
+  glEnd
+  
+  writeIORef xrot $! xr + 0.3
+  writeIORef yrot $! yr + 0.2
+  writeIORef zrot $! zr + 0.4
 
-  -- since this is double buffered, swap the buffers to display what was just
-  -- drawn
-  flush
-  swapBuffers
+  glFlush
 
+shutdown :: GLFW.WindowCloseCallback
+shutdown = do
+  GLFW.closeWindow
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return True
 
-keyPressed :: KeyboardMouseCallback
--- 27 is ESCAPE
-keyPressed (Char '\27') Down _ _ = exitWith ExitSuccess
-keyPressed _            _    _ _ = return ()
+keyPressed :: GLFW.KeyCallback
+keyPressed GLFW.KeyEsc True = shutdown >> return ()
+keyPressed _           _    = return ()
 
 main :: IO ()
 main = do
-     -- Initialize GLUT state - glut will take any command line arguments
-     -- that pertain to it or X windows -- look at its documentation at
-     -- http://reality.sgi.com/mjk/spec3/spec3.html
-     _ <- getArgsAndInitialize
+     True <- GLFW.initialize
      -- select type of display mode:
      -- Double buffer
      -- RGBA color
      -- Alpha components supported
      -- Depth buffer
-     initialDisplayMode $= [ DoubleBuffered, RGBAMode, WithDepthBuffer, 
-                             WithAlphaComponent ]
-     -- get an 800 x 600 window
-     initialWindowSize $= Size 800 600
-     -- window starts at upper left corner of the screen
-     initialWindowPosition $= Position 0 0
+     let dspOpts = GLFW.defaultDisplayOptions
+                     -- get a 800 x 600 window
+                     { GLFW.displayOptions_width  = 800
+                     , GLFW.displayOptions_height = 600
+                     -- Set depth buffering and RGBA colors
+                     , GLFW.displayOptions_numRedBits   = 8
+                     , GLFW.displayOptions_numGreenBits = 8
+                     , GLFW.displayOptions_numBlueBits  = 8
+                     , GLFW.displayOptions_numAlphaBits = 8
+                     , GLFW.displayOptions_numDepthBits = 1
+                     -- , GLFW.displayOptions_displayMode = GLFW.Fullscreen
+                     }
      -- open a window
-     _ <- createWindow "Jeff Molofee's GL Code Tutorial ... NeHe '99"
+     True <- GLFW.openWindow dspOpts
+     -- window starts at upper left corner of the screen
+     GLFW.setWindowPosition 0 0
+     GLFW.setWindowTitle "Jeff Molofee's GL Code Tutorial ... NeHe '99"
      -- register the function to do all our OpenGL drawing
      xrot <- newIORef 0
      yrot <- newIORef 0
      zrot <- newIORef 0
-     -- initialize our window.
-     tex <- initGL
-     displayCallback $= (drawScene tex xrot yrot zrot)
-     -- go fullscreen. This is as soon as possible.
-     fullScreen
-     -- even if there are no events, redraw our gl scene
-     idleCallback $= Just (drawScene tex xrot yrot zrot)
+     tex  <- initGL
+     GLFW.setWindowRefreshCallback (drawScene tex xrot yrot zrot)
      -- register the funciton called when our window is resized
-     reshapeCallback $= Just resizeScene
+     GLFW.setWindowSizeCallback resizeScene
      -- register the function called when the keyboard is pressed.
-     keyboardMouseCallback $= Just keyPressed
+     GLFW.setKeyCallback keyPressed
+     GLFW.setWindowCloseCallback shutdown
+     -- initialize our window.
      -- start event processing engine
-     mainLoop
+     forever $ do
+       threadDelay 1
+       drawScene tex xrot yrot zrot
+       GLFW.swapBuffers
