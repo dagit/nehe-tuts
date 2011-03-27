b0f904364835d6db499510734e5255bdeb5bc38c
Jason Dagit <dagitj@gmail.com>
2011-03-27 16:12:43 -0700

    Update lesson12 Refactor cabal file

--- lesson12.hs
+++ lesson12.hs
@@ -4,163 +4,189 @@
 
 module Main where
 
-import Graphics.UI.GLUT 
+import qualified Graphics.UI.GLFW as GLFW
+-- everything from here starts with gl or GL
+import Graphics.Rendering.OpenGL.Raw
+import Graphics.Rendering.GLU.Raw ( gluPerspective )
+import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
-import Data.IORef ( IORef, newIORef, modifyIORef )
+import Control.Monad ( forever, forM_ )
+import Data.IORef ( IORef, newIORef, readIORef, modifyIORef )
+import Foreign ( withForeignPtr, plusPtr, alloca, peek )
+import qualified Data.ByteString.Internal as BSI
 import Util ( Image(..), bitmapLoad )
-import Monad ( liftM )
 
-boxcol :: [Color3 GLfloat]
-boxcol = [Color3 1 0 0, Color3 1 0.5 0, Color3 1 1 0, 
-          Color3 0 1 0, Color3 0 1 1]
-topcol :: [Color3 GLfloat]
-topcol = [Color3 0.5 0 0, Color3 0.5 0.25 0, Color3 0.5 0.5 0,
-          Color3 0 0.5 0, Color3 0 0.5 0.5]
+boxcol :: [(GLfloat, GLfloat, GLfloat)]
+boxcol = [(1, 0, 0), (1, 0.5, 0), (1, 1, 0), 
+          (0, 1, 0), (0, 1, 1)]
+topcol :: [(GLfloat, GLfloat, GLfloat)]
+topcol = [(0.5, 0, 0), (0.5, 0.25, 0), (0.5, 0.5, 0),
+          (0, 0.5, 0), (0, 0.5, 0.5)]
 
-buildLists :: IO (DisplayList, DisplayList)
+buildLists :: IO (GLuint, GLuint)
 buildLists = do
-  box <- defineNewList Compile $ 
-    do renderPrimitive Quads $ 
-         do { texCoord (TexCoord2 1 (1::GLfloat)); vertex (Vertex3 (-1) (-1) (-1::GLfloat));
-              texCoord (TexCoord2 0.0 (1.0::GLfloat)); vertex(Vertex3 1.0 (-1.0) (-1.0::GLfloat));      -- Top Left Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (0.0::GLfloat)); vertex(Vertex3 1.0 (-1.0)  (1.0::GLfloat));      -- Bottom Left Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (0.0::GLfloat)); vertex(Vertex3(-1.0) (-1.0)  (1.0::GLfloat));      -- Bottom Right Of The Texture and Quad
-              -- Front Face
-              texCoord (TexCoord2 0.0 (0.0::GLfloat)); vertex(Vertex3(-1.0) (-1.0)  (1.0::GLfloat));      -- Bottom Left Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (0.0::GLfloat)); vertex(Vertex3 1.0 (-1.0)  (1.0::GLfloat));      -- Bottom Right Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (1.0::GLfloat)); vertex(Vertex3 1.0  1.0  (1.0::GLfloat));      -- Top Right Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (1.0::GLfloat)); vertex(Vertex3(-1.0)  1.0  (1.0::GLfloat));      -- Top Left Of The Texture and Quad
-              -- Back Face
-              texCoord (TexCoord2 1.0 (0.0::GLfloat)); vertex(Vertex3(-1.0) (-1.0) (-1.0::GLfloat));      -- Bottom Right Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (1.0::GLfloat)); vertex(Vertex3(-1.0)  1.0 (-1.0::GLfloat));      -- Top Right Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (1.0::GLfloat)); vertex(Vertex3 1.0  1.0 (-1.0::GLfloat));      -- Top Left Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (0.0::GLfloat)); vertex(Vertex3 1.0 (-1.0) (-1.0::GLfloat));      -- Bottom Left Of The Texture and Quad
-              -- Right face
-              texCoord (TexCoord2 1.0 (0.0::GLfloat)); vertex(Vertex3 1.0 (-1.0) (-1.0::GLfloat));      -- Bottom Right Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (1.0::GLfloat)); vertex(Vertex3 1.0  1.0 (-1.0::GLfloat));      -- Top Right Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (1.0::GLfloat)); vertex(Vertex3 1.0  1.0  (1.0::GLfloat));      -- Top Left Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (0.0::GLfloat)); vertex(Vertex3 1.0 (-1.0)  (1.0::GLfloat));      -- Bottom Left Of The Texture and Quad
-              -- Left Face
-              texCoord (TexCoord2 0.0 (0.0::GLfloat)); vertex(Vertex3(-1.0) (-1.0) (-1.0::GLfloat));      -- Bottom Left Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (0.0::GLfloat)); vertex(Vertex3(-1.0) (-1.0)  (1.0::GLfloat));      -- Bottom Right Of The Texture and Quad
-              texCoord (TexCoord2 1.0 (1.0::GLfloat)); vertex(Vertex3(-1.0)  1.0  (1.0::GLfloat));      -- Top Right Of The Texture and Quad
-              texCoord (TexCoord2 0.0 (1.0::GLfloat)); vertex(Vertex3(-1.0)  1.0 (-1.0::GLfloat));}      -- Top Left Of The Texture and Quad
-  top <- defineNewList Compile $ 
-    do renderPrimitive Quads $ 
-         do { texCoord (TexCoord2 0 (1::GLfloat)); vertex (Vertex3 (-1) 1 (-1::GLfloat));
-              texCoord (TexCoord2 0 (0::GLfloat)); vertex (Vertex3 (-1) 1 (1::GLfloat));
-              texCoord (TexCoord2 1 (0::GLfloat)); vertex (Vertex3 1 1 (1::GLfloat));
-              texCoord (TexCoord2 1 (1::GLfloat)); vertex (Vertex3 1 1 (-1::GLfloat)); }
+  box <- glGenLists 2
+  glNewList box gl_COMPILE
+  glBegin gl_QUADS
+  glTexCoord2f 1   1   >> glVertex3f   (-1) (-1)   (-1)   -- Top Right Of The Texture and Quad
+  glTexCoord2f 0.0 1.0 >> glVertex3f   1.0  (-1.0) (-1.0) -- Top Left Of The Texture and Quad
+  glTexCoord2f 0.0 0.0 >> glVertex3f   1.0  (-1.0)  1.0   -- Bottom Left Of The Texture and Quad
+  glTexCoord2f 1.0 0.0 >> glVertex3f (-1.0) (-1.0)  1.0   -- Bottom Right Of The Texture and Quad
+  -- Front Face
+  glTexCoord2f 0.0 0.0 >> glVertex3f (-1.0) (-1.0)  1.0   -- Bottom Left Of The Texture and Quad
+  glTexCoord2f 1.0 0.0 >> glVertex3f   1.0  (-1.0)  1.0   -- Bottom Right Of The Texture and Quad
+  glTexCoord2f 1.0 1.0 >> glVertex3f   1.0    1.0   1.0   -- Top Right Of The Texture and Quad
+  glTexCoord2f 0.0 1.0 >> glVertex3f (-1.0)   1.0   1.0   -- Top Left Of The Texture and Quad
+  -- Back Face
+  glTexCoord2f 1.0 0.0 >> glVertex3f (-1.0) (-1.0) (-1.0) -- Bottom Right Of The Texture and Quad
+  glTexCoord2f 1.0 1.0 >> glVertex3f (-1.0)   1.0  (-1.0) -- Top Right Of The Texture and Quad
+  glTexCoord2f 0.0 1.0 >> glVertex3f   1.0    1.0  (-1.0) -- Top Left Of The Texture and Quad
+  glTexCoord2f 0.0 0.0 >> glVertex3f   1.0  (-1.0) (-1.0) -- Bottom Left Of The Texture and Quad
+  -- Right face
+  glTexCoord2f 1.0 0.0 >> glVertex3f   1.0  (-1.0) (-1.0) -- Bottom Right Of The Texture and Quad
+  glTexCoord2f 1.0 1.0 >> glVertex3f   1.0    1.0  (-1.0) -- Top Right Of The Texture and Quad
+  glTexCoord2f 0.0 1.0 >> glVertex3f   1.0    1.0    1.0  -- Top Left Of The Texture and Quad
+  glTexCoord2f 0.0 0.0 >> glVertex3f   1.0  (-1.0)   1.0  -- Bottom Left Of The Texture and Quad
+  -- Left Face
+  glTexCoord2f 0.0 0.0 >> glVertex3f (-1.0) (-1.0) (-1.0) -- Bottom Left Of The Texture and Quad
+  glTexCoord2f 1.0 0.0 >> glVertex3f (-1.0) (-1.0)   1.0  -- Bottom Right Of The Texture and Quad
+  glTexCoord2f 1.0 1.0 >> glVertex3f (-1.0)   1.0    1.0  -- Top Right Of The Texture and Quad
+  glTexCoord2f 0.0 1.0 >> glVertex3f (-1.0)   1.0  (-1.0) -- Top Left Of The Texture and Quad
+
+  glEndList
+  let top = box + 1
+  glNewList top gl_COMPILE
+  glBegin gl_QUADS
+  glTexCoord2f 0 1 >> glVertex3f (-1) 1 (-1)
+  glTexCoord2f 0 0 >> glVertex3f (-1) 1   1
+  glTexCoord2f 1 0 >> glVertex3f   1  1   1
+  glTexCoord2f 1 1 >> glVertex3f   1  1 (-1)
+  glEnd
+  glEndList
   return (box, top)
 
-initGL :: IO TextureObject
+initGL :: IO GLuint
 initGL = do
-  tex <- loadGLTextures
-  texture Texture2D $= Enabled
-  clearColor $= Color4 0 0 0 0.5 -- Clear the background color to black
-  clearDepth $= 1 -- enables clearing of the depth buffer
-  depthFunc  $= Just Less -- type of depth test
-  shadeModel $= Smooth -- enables smooth color shading
-  matrixMode $= Projection
-  hint PerspectiveCorrection $= Nicest
-  loadIdentity  -- reset projection matrix
-  Size width height <- get windowSize
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100 -- calculate the aspect ratio of the window
-  matrixMode $= Modelview 0
-  light (Light 0) $= Enabled
-  lighting $= Enabled
-  colorMaterial $= Just (FrontAndBack, AmbientAndDiffuse)
-  flush -- finally, we tell opengl to do it.
+  tex <- loadTextures
+  glEnable gl_TEXTURE_2D
+  glShadeModel gl_SMOOTH
+  glClearColor 0 0 0 0.5
+  glClearDepth 1
+  glEnable gl_DEPTH_TEST
+  glEnable gl_LEQUAL
+  glEnable gl_LIGHT0
+  glEnable gl_LIGHTING
+  glEnable gl_COLOR_MATERIAL
+  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
   return tex
 
-loadGLTextures :: IO TextureObject
-loadGLTextures = do
-  (Image (Size w h) pd) <- bitmapLoad "Data/cube.bmp"
-  texName <- liftM head (genObjectNames 1)
-  textureBinding Texture2D $= Just texName
-  textureFilter  Texture2D $= ((Nearest, Nothing), Nearest)
-  texImage2D Nothing NoProxy 0 RGB' (TextureSize2D w h) 0 pd
-  return texName
+loadTextures :: IO GLuint
+loadTextures = do
+  Just (Image w h pd) <- bitmapLoad "Data/cube.bmp"
+  putStrLn $ "Image w = " ++ show w
+  putStrLn $ "Image h = " ++ show h
+  tex <- alloca $ \p -> do
+            glGenTextures 1 p
+            peek p
+  let (ptr, off, _) = BSI.toForeignPtr pd
+  _ <- withForeignPtr ptr $ \p -> do
+    let p' = p `plusPtr` off
+        glNearest  = fromIntegral gl_NEAREST
+    -- create linear filtered texture
+    glBindTexture gl_TEXTURE_2D tex
+    glTexImage2D gl_TEXTURE_2D 0 3
+      (fromIntegral w) (fromIntegral h)
+      0 gl_RGB gl_UNSIGNED_BYTE p'
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MAG_FILTER glNearest
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MIN_FILTER glNearest
+  return tex
 
-resizeScene :: Size -> IO ()
-resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
-resizeScene s@(Size width height) = do
-  viewport   $= (Position 0 0, s)
-  matrixMode $= Projection
-  loadIdentity
-  perspective 45 (fromIntegral width/fromIntegral height) 0.1 100
-  matrixMode $= Modelview 0
-  flush
+shutdown :: GLFW.WindowCloseCallback
+shutdown = do
+  GLFW.closeWindow
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return True
 
-drawScene :: TextureObject -> IORef GLfloat -> IORef GLfloat
-             -> DisplayList -> DisplayList -> IO ()
+resizeScene :: GLFW.WindowSizeCallback
+resizeScene w     0      = resizeScene w 1 -- prevent divide by zero
+resizeScene width height = do
+  glViewport 0 0 (fromIntegral width) (fromIntegral height)
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100
+  glMatrixMode gl_MODELVIEW
+  glLoadIdentity
+  glFlush
+
+drawScene :: GLuint -> IORef GLfloat -> IORef GLfloat
+          -> GLuint -> GLuint -> IO ()
 drawScene tex xrot yrot box top = do
-  clear [ColorBuffer, DepthBuffer] -- clear the screen and the depth bufer
-  textureBinding Texture2D $= Just tex
+  glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
+                         .|. gl_DEPTH_BUFFER_BIT
+  glBindTexture gl_TEXTURE_2D tex
 
-  xr <- get xrot
-  yr <- get yrot
+  xr <- readIORef xrot
+  yr <- readIORef yrot
 
-  mapM_ (\(x,y) -> 
-    do { loadIdentity;
-         let {x' = fromIntegral x;
-              y' = fromIntegral y };
-         translate (Vector3 (1.4+x'*2.8-y'*1.4) (((6-y')*2.4)-7)  (-20::GLfloat));
-         rotate (45.0-(2.0*y')+xr) (Vector3 1 0 (0::GLfloat));
-         rotate (45-yr) (Vector3 0 1 (0::GLfloat));
-         color (boxcol !! (y-1));
-         callList box;
-         color (topcol !! (y-1));
-         callList top } ) [(x,y) | y <- [1..5], x <- [0..y-1] ]
+  forM_ [(x,y) | y <- [1..5], x <- [0..y-1] ] $ \(x,y) -> do
+    glLoadIdentity
+    let x' = fromIntegral x
+        y' = fromIntegral y
+        color (r,g,b) = glColor3f r g b
+    glTranslatef (1.4+x'*2.8-y'*1.4) (((6-y')*2.4)-7)  (-20)
+    glRotatef (45.0-(2.0*y')+xr) 1 0 0
+    glRotatef (45-yr) 0 1 0
+    color (boxcol !! (y-1))
+    glCallList box
+    color (topcol !! (y-1))
+    glCallList top 
+  glFlush
 
-  -- since this is double buffered, swap the buffers to display what was just
-  -- drawn
-  flush
-  swapBuffers
-
-keyPressed :: IORef GLfloat -> IORef GLfloat -> KeyboardMouseCallback
--- 27 is ESCAPE
-keyPressed _ _ (Char '\27') Down _ _ = exitWith ExitSuccess
-keyPressed xrot _ (SpecialKey KeyUp) Down _ _ = modifyIORef xrot (subtract 0.8)
-keyPressed xrot _ (SpecialKey KeyDown) Down _ _ = modifyIORef xrot (+0.8)
-keyPressed _ yrot (SpecialKey KeyLeft) Down _ _ = modifyIORef yrot (subtract 0.8)
-keyPressed _ yrot (SpecialKey KeyRight) Down _ _ = modifyIORef yrot (+0.8)
-keyPressed _ _ _            _    _ _ = return ()
+keyPressed :: IORef GLfloat -> IORef GLfloat -> GLFW.KeyCallback
+keyPressed _    _    GLFW.KeyEsc   True = shutdown >> return ()
+keyPressed xrot _    GLFW.KeyUp    True = modifyIORef xrot (subtract 0.8)
+keyPressed xrot _    GLFW.KeyDown  True = modifyIORef xrot (+0.8)
+keyPressed _    yrot GLFW.KeyLeft  True = modifyIORef yrot (subtract 0.8)
+keyPressed _    yrot GLFW.KeyRight True = modifyIORef yrot (+0.8)
+keyPressed _    _    _             _    = return ()
 
 main :: IO ()
 main = do
-     -- Initialize GLUT state - glut will take any command line arguments
-     -- that pertain to it or X windows -- look at its documentation at
-     -- http://reality.sgi.com/mjk/spec3/spec3.html
-     _ <- getArgsAndInitialize
+     True <- GLFW.initialize
      -- select type of display mode:
      -- Double buffer
      -- RGBA color
      -- Alpha components supported
      -- Depth buffer
-     initialDisplayMode $= [ DoubleBuffered, RGBAMode, WithDepthBuffer, 
-                             WithAlphaComponent ]
-     -- get an 800 x 600 window
-     initialWindowSize $= Size 800 600
-     -- window starts at upper left corner of the screen
-     initialWindowPosition $= Position 0 0
+     let dspOpts = GLFW.defaultDisplayOptions
+                     -- get a 800 x 600 window
+                     { GLFW.displayOptions_width  = 800
+                     , GLFW.displayOptions_height = 600
+                     -- Set depth buffering and RGBA colors
+                     , GLFW.displayOptions_numRedBits   = 8
+                     , GLFW.displayOptions_numGreenBits = 8
+                     , GLFW.displayOptions_numBlueBits  = 8
+                     , GLFW.displayOptions_numAlphaBits = 8
+                     , GLFW.displayOptions_numDepthBits = 24
+                     -- , GLFW.displayOptions_displayMode = GLFW.Fullscreen
+                     }
      -- open a window
-     _ <- createWindow "Jeff Molofee's GL Code Tutorial ... NeHe '99"
-     -- register the function to do all our OpenGL drawing
+     True <- GLFW.openWindow dspOpts
      xrot <- newIORef 0
      yrot <- newIORef 0
      
      -- initialize our window.
      tex <- initGL
      (box, top) <- buildLists
-     displayCallback $= (drawScene tex xrot yrot box top)
-     -- go fullscreen. This is as soon as possible.
-     fullScreen
-     -- even if there are no events, redraw our gl scene
-     idleCallback $= Just (drawScene tex xrot yrot box top)
-     -- register the funciton called when our window is resized
-     reshapeCallback $= Just resizeScene
+     GLFW.setWindowRefreshCallback $
+       drawScene tex xrot yrot box top
+     GLFW.setWindowSizeCallback resizeScene
      -- register the function called when the keyboard is pressed.
-     keyboardMouseCallback $= Just (keyPressed xrot yrot)
-     -- start event processing engine
-     mainLoop
+     GLFW.setKeyCallback $
+       keyPressed xrot yrot
+     GLFW.setWindowCloseCallback shutdown
+     GLFW.getWindowRefreshRate >>= print
+     forever $ do
+       drawScene tex xrot yrot box top
+       GLFW.swapBuffers
